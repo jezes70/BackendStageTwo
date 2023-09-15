f48ed40dd08f1aaee65f21ac765dbfa6505cf76d
jezes70 <jezes70@gmail.comm>
2023-09-15 02:27:21 +0100

    init

--- src/config/database.ts
+++ src/config/database.ts
@@ -11,34 +11,3 @@ export const connect = async () => {
     console.log(err);
   }
 };
-
-// import mongoose from "mongoose";
-// import dotenv from "dotenv";
-
-// dotenv.config();
-
-// const { MONGODB_URL, MONGODB_OPTIONS } = process.env;
-
-// const db = async (): Promise<void> => {
-//   try {
-//     await mongoose.connect("MONGODB_URL", {});
-//     console.log("MongoDB connected Successfully");
-//   } catch (err) {
-//     console.error(`MongoDB connection error: ${err}`);
-//     process.exit(1);
-//   }
-// };
-// export default db;
-
-// import mongoose, { ConnectOptions } from "mongoose";
-// const MONGODB_URI = process.env.MONGODB_URI || "mongodb://localhost:27017/";
-// const db = async (): Promise<void> => {
-//   try {
-//     await mongoose.connect(MONGODB_URI, {});
-//     console.log("MongoDB connected Successfully");
-//   } catch (err) {
-//     console.error(`MongoDB connection error: ${err}`);
-//     process.exit(1);
-//   }
-// };
-// export default db;
--- src/model/person.ts
+++ src/model/person.ts
@@ -2,12 +2,10 @@ import mongoose, { Document, Schema } from "mongoose";
 
 interface IPerson extends Document {
   name: string;
-  // Add more fields here as needed
 }
 
 const personSchema = new Schema<IPerson>({
   name: { type: String, required: true },
-  // Define other fields here
 });
 
 const Person = mongoose.model<IPerson>("Person", personSchema);
